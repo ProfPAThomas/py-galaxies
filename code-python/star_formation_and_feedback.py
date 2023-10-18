2a43a4cb29ba2381e15e0d11246afc23027aefb2
Peter Thomas <p.a.thomas@sussex.ac.uk>
2023-10-18 14:09:55 +0100

    Fixed feedback to properly reflect Hen15

--- code-python/star_formation_and_feedback.py
+++ code-python/star_formation_and_feedback.py
@@ -153,31 +153,35 @@ def F_gal_SNR_feedback(mass_stars,gal,sub,halo,parameters):
    snr_model=parameters.snr_model
    if snr_model == "Hen15":
       # Is it better to pass the reheating parameters that we need individually?
-      mass_halo, mass_eject = F_SNR_feedback_Hen15(mass_stars,sub.half_mass_virial_speed,gal['mass_gas_cold'],parameters)
+      mass_reheat, mass_eject_max = F_SNR_feedback_Hen15(mass_stars,sub.half_mass_virial_speed,gal['mass_gas_cold'],parameters)
    else:
       raise valueError('snr model '+snr_model+' not yet implemented')
-   mass_reheat = mass_halo + mass_eject
-   #print('mass_halo, mass_eject =',mass_halo,mass_eject)
 
    # Need generic routine (as in L-Galaxies) to do these mass transfers, especially when get many components
-   Zmet = gal['mass_metals_gas_cold'] / gal['mass_gas_cold']
-   mass_metals_halo = mass_halo * Zmet
-   mass_metals_eject = mass_eject * Zmet
-   mass_metals_reheat = mass_reheat * Zmet
-   gal['mass_gas_cold'] -= mass_reheat
-   gal['mass_metals_gas_cold'] -= mass_metals_reheat
-   sub.mass_gas_hot += mass_halo
-   sub.mass_metals_gas_hot += mass_metals_halo
-   gal['mass_baryon'] -= mass_reheat
-   # Ejected gas leaves the subhalo (but not the halo, if the subhalo is a halo)
-   try:
-        sub.sub_sid
-        sub.mass_baryon -= mass_eject
-   except:
-        pass
-   halo.mass_gas_eject += mass_eject
-   halo.mass_metals_gas_eject += mass_metals_eject
-   
+   if mass_reheat > parameters.mass_minimum_internal:
+      Zmet = gal['mass_metals_gas_cold'] / gal['mass_gas_cold']
+      mass_metals_reheat = mass_reheat * Zmet
+      gal['mass_gas_cold'] -= mass_reheat
+      gal['mass_metals_gas_cold'] -= mass_metals_reheat
+      sub.mass_gas_hot += mass_reheat
+      sub.mass_metals_gas_hot += mass_metals_reheat
+      gal['mass_baryon'] -= mass_reheat
+   # The following assumes full mixing of metals before subsequent ejection
+   mass_eject=min(mass_eject_max,sub.mass_gas_hot.copy())
+   if mass_eject > parameters.mass_minimum_internal:
+      Zmet = sub.mass_metals_gas_hot / sub.mass_gas_hot
+      mass_metals_eject = mass_eject * Zmet
+      sub.mass_gas_hot -= mass_eject
+      sub.mass_metals_gas_hot -= mass_metals_eject
+      halo.mass_gas_eject += mass_eject
+      halo.mass_metals_gas_eject += mass_metals_eject
+      # Ejected gas leaves the subhalo (but not the halo, if the subhalo is a halo)
+      try:
+         sub.sub_sid
+         sub.mass_baryon -= mass_eject
+      except:
+         pass
+
    return None
 
 #-------------------------------------------------------------------------------------------------------------
@@ -207,18 +211,16 @@ def F_SNR_feedback_Hen15(mass_stars,v_vir,mass_gas_cold,parameters):
    """
    
    # Eq S16 & S17 SNR feedback energy (note: have absorbed minus sign into ratio by swapping num. & denom.)
-   epsilon_halo = parameters.Hen15_eta*(0.5+(parameters.Hen15_v_eject_internal/v_vir)**parameters.Hen15_beta2)
-   DE_snr = parameters.c_Hen15_S16*epsilon_halo*mass_stars
+   epsilon_feedback = parameters.Hen15_eta*(0.5+(parameters.Hen15_v_eject_internal/v_vir)**parameters.Hen15_beta2)
+   DE_feedback = parameters.c_Hen15_S16*epsilon_feedback*mass_stars
    # Amount of mass that this energy can raise to the virial temperature (well, actually, 0.5*v_vir^2)
-   DM_snr = DE_snr / (0.5 * v_vir**2)
+   DM_feedback = DE_feedback / (0.5 * v_vir**2)
    # Eq S18 & S19 SNR reheated mass (note: have absorbed minus sign into ratio by swapping num. & denom.)
-   epsilon_disk = parameters.Hen15_epsilon*(0.5+(parameters.Hen15_v_reheat_internal/v_vir)**parameters.Hen15_beta1)
-   DM_reheat_max = epsilon_disk*mass_stars
+   mu_reheat_max = parameters.Hen15_epsilon*(0.5+(parameters.Hen15_v_reheat_internal/v_vir)**parameters.Hen15_beta1)
+   DM_reheat_max = mu_reheat_max*mass_stars
    # Can't reheat more gas than exists, or that we have energy for
-   DM_reheat = min(DM_snr,DM_reheat_max,mass_gas_cold)
+   DM_reheat = min(DM_feedback,DM_reheat_max,mass_gas_cold)
    # Use excess energy to eject gas from the (sub)halo
-   DM_eject = min(DM_snr-DM_reheat,DM_reheat)
-   DM_halo = DM_reheat - DM_eject
+   DM_eject_max = DM_feedback-DM_reheat
     
-   return DM_halo, DM_eject
- 
+   return DM_reheat, DM_eject_max
